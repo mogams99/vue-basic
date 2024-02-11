8afb71b94a3dd03fcc684a59cb0ca34cb4cf3f83
Mochammad Gamal <mogams1811@gmail.com>
2024-02-11 14:24:28 +0700

    chore: :hammer: chapter 1 - menampilkan produk dengan looping v-for

--- chapter-1/app.js
+++ chapter-1/app.js
@@ -1,11 +1,44 @@
-var product = {
-    "name": "Product 1",
-    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-    "price": "100.000",
-    "image": "https://placehold.co/300x300"
+var dataProduct = {
+    products: [
+        {
+            "id": "1",
+            "name": "Product 1",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+            "price": "1.00",
+            "image": "https://placehold.co/300x300"
+        },
+        {
+            "id": "2",
+            "name": "Product 2",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+            "price": "2.00",
+            "image": "https://placehold.co/300x300"
+        },
+        {
+            "id": "3",
+            "name": "Product 3",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+            "price": "3.00",
+            "image": "https://placehold.co/300x300"
+        },
+        {
+            "id": "4",
+            "name": "Product 4",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+            "price": "4.00",
+            "image": "https://placehold.co/300x300"
+        },
+        {
+            "id": "5",
+            "name": "Product 5",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+            "price": "5.00",
+            "image": "https://placehold.co/300x300"
+        },
+    ]
 }
 
 var app = new Vue({
     el: '#app',
-    data: product
+    data: dataProduct
 });
