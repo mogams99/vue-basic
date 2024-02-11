99dba11f853803dd1fa1eb2b76867b57714d0b5e
Mochammad Gamal <mogams1811@gmail.com>
2024-02-11 14:11:24 +0700

    chore: :hammer: chapter 1 - binding data pada atribut HTML

--- chapter-1/app.js
+++ chapter-1/app.js
@@ -1,6 +1,11 @@
+var product = {
+    "name": "Product 1",
+    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
+    "price": "100.000",
+    "image": "https://placehold.co/300x300"
+}
+
 var app = new Vue({
     el: '#app',
-    data: {
-        name: 'Vue.js'
-    }
+    data: product
 });
