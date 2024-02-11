8cc53880aeccb68c13ad01541b5cbaa191001654
Mochammad Gamal <mogams1811@gmail.com>
2024-02-11 15:32:03 +0700

    chore: :hammer: chapter 1 - implementasi mounted lifecycle hooks vue js

--- chapter-1/app.js
+++ chapter-1/app.js
@@ -1,45 +1,14 @@
-var dataProduct = {
-    maximum: 50,
-    products: [
-        {
-            "id": "1",
-            "name": "Product 1",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-            "price": "31.00",
-            "image": "https://placehold.co/300x300"
-        },
-        {
-            "id": "2",
-            "name": "Product 2",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-            "price": "112.00",
-            "image": "https://placehold.co/300x300"
-        },
-        {
-            "id": "3",
-            "name": "Product 3",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-            "price": "13.00",
-            "image": "https://placehold.co/300x300"
-        },
-        {
-            "id": "4",
-            "name": "Product 4",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-            "price": "24.00",
-            "image": "https://placehold.co/300x300"
-        },
-        {
-            "id": "5",
-            "name": "Product 5",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium architecto distinctio nobis doloremque eligendi, laboriosam qui commodi sunt. Praesentium, culpa nemo. Rem optio molestiae, ab earum excepturi exercitationem beatae corrupti!",
-            "price": "52.00",
-            "image": "https://placehold.co/300x300"
-        },
-    ]
-}
-
 var app = new Vue({
     el: '#app',
-    data: dataProduct
+    data: {
+        maximum: 50,
+        products: null,
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
+    }
 });
