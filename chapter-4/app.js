ad72fc65710a1bd37369a4ce708ac8c74d99b3a7
Mochammad Gamal <mogams1811@gmail.com>
2024-02-12 15:52:35 +0700

    chore: :hammer: chapter 4 - intro filters property

---
+++ chapter-4/app.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        style: {
+            label: ['fw-bold', 'mr-2'],
+            inputWidth: 60,
+            sliderStatus: false,
+        },
+        maximum: 50,
+        imgClass: 'img-fluid d-block',
+        products: null,
+        cart: [],
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
+    },
+    filters: {
+        currencyFormat: function (value) {
+            return 'Rp' + Number.parseFloat(value).toFixed(2);
+        }
+    },
+    computed: {
+        sliderState: function() {
+            return this.style.sliderStatus ? 'd-flex mt-5' : 'd-none';
+        }
+    },
+    methods: {
+        before: function(el) {
+            el.className = 'd-none';
+        },
+        enter: function(el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeInRight'
+            }, delay);
+        },
+        leave: function(el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeOutRight'
+            }, delay);
+        },
+        addItem: function(product) {
+            this.cart.push(product)
+        }
+    },
+});
