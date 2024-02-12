894fad212a2360bdea08022a9369029354273019
Mochammad Gamal <mogams1811@gmail.com>
2024-02-12 09:55:47 +0700

    chore: :hammer: chapter 3 - binding atribut dengan array dan objek yang berisi class atau style css

---
+++ chapter-3/app.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        maximum: 50,
+        imgClass: 'img-fluid d-block',
+        products: null,
+        cart: [],
+        style: {
+            label: ['fw-bold', 'mr-2'],
+        }
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
+    },
+    methods: {
+        addItem: function(product) {
+            console.warn(product);
+            this.cart.push(product)
+        }
+    }
+});
