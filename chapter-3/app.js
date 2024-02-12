bfaa74a1d095e19e42bc548290cd18e6d3d9c68d
Mochammad Gamal <mogams1811@gmail.com>
2024-02-12 10:13:20 +0700

    chore: :hammer: chapter 3 - programmed styling vue js

--- chapter-3/app.js
+++ chapter-3/app.js
@@ -1,13 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
+        style: {
+            label: ['fw-bold', 'mr-2'],
+            inputWidth: 60,
+            sliderStatus: false,
+        },
         maximum: 50,
         imgClass: 'img-fluid d-block',
         products: null,
         cart: [],
-        style: {
-            label: ['fw-bold', 'mr-2'],
-        }
     },
     mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
@@ -16,10 +18,15 @@ var app = new Vue({
             this.products = data;
         });
     },
+    computed: {
+        sliderState: function() {
+            return this.style.sliderStatus ? 'd-flex mt-5' : 'd-none';
+        }
+    },
     methods: {
         addItem: function(product) {
             console.warn(product);
             this.cart.push(product)
         }
-    }
+    },
 });
