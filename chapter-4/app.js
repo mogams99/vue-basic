4830dc37e84f1341fc5021328a29be0c16c2e3a5
Mochammad Gamal <mogams1811@gmail.com>
2024-03-03 17:19:15 +0700

    chore: :hammer: chapter 4 - pengkodisian array untuk fitur tambah keranjang

--- chapter-4/app.js
+++ chapter-4/app.js
@@ -45,7 +45,23 @@ var app = new Vue({
             }, delay);
         },
         addItem: function(product) {
-            this.cart.push(product)
+            console.warn(product.id);
+
+            var productIndex;
+            var productExist = this.cart.filter(function(item, index) {
+                if (item.product.id == Number(product.id)) {
+                    productIndex = index;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (productExist.length) {
+                this.cart[productIndex].qty++
+            } else {
+                this.cart.push({product: product, qty: 1});
+            }
         }
     },
 });
