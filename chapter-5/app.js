406f98dd7c6d6ebdb468d101bc9e5f5ee6113133
Mochammad Gamal <mogams1811@gmail.com>
2024-03-04 08:42:54 +0700

    chore: :hammer: chapter 5 - setup default value pada props

--- chapter-5/app.js
+++ chapter-5/app.js
@@ -2,7 +2,17 @@ Vue.component('price', {
     data: function() {
         return {}
     },
-    props: ['value', 'prefix', 'precision'],
+    props: {
+        value: Number,
+        prefix: {
+            type: String,
+            default: 'Rp'
+        },
+        precision: {
+            type: Number,
+            default: 2
+        }
+    },
     template: '<span>{{ this.prefix + Number.parseFloat(this.value).toFixed(this.precision) }}</span>'
 });
 
