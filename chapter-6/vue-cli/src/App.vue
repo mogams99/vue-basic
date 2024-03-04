2e9d3dad0f4ba4358934c3b41891ad753ef5dda5
Mochammad Gamal <mogams1811@gmail.com>
2024-03-04 14:51:12 +0700

    chore: :hammer: chapter 6 - implementasi beberapa modul yang sudah di install

--- chapter-6/vue-cli/src/App.vue
+++ chapter-6/vue-cli/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>VueShop</h1>
+    <p class="animate__animated animate__fadeInTopRight">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde doloribus
+      provident dolorem cum qui alias in facere placeat commodi reiciendis
+      nostrum ea velit totam pariatur nisi, voluptas ex suscipit perspiciatis!
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- chapter-6/vue-cli/src/main.js
+++ chapter-6/vue-cli/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import "animate.css/animate.css";
+import "animate.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
