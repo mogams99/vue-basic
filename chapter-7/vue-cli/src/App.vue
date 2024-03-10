6d3ff447eed4dce6dd1eec1000bb853d0fd05b8b
Mochammad Gamal <mogams1811@gmail.com>
2024-03-10 20:45:18 +0700

    chore: :hammer: chapter 7 - product list component

--- chapter-7/vue-cli/src/App.vue
+++ chapter-7/vue-cli/src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>VueShop</h1>
-    <p class="animate__animated animate__fadeInTopRight">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde doloribus
-      provident dolorem cum qui alias in facere placeat commodi reiciendis
-      nostrum ea velit totam pariatur nisi, voluptas ex suscipit perspiciatis!
-    </p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-    <price :value="4.23"></price>
+    <product-list :maximum="maximum" :products="products"></product-list>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import Price from "./components/Price.vue";
+import ProductList from "./components/ProductList.vue";
 
 export default {
   name: "App",
+  data: function () {
+    return {
+      maximum: 20,
+      products: [],
+    };
+  },
   components: {
-    FontAwesomeIcon,
-    Price,
+    ProductList,
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
   },
 };
 </script>
--- chapter-7/vue-cli/src/main.js
+++ chapter-7/vue-cli/src/main.js
@@ -4,9 +4,9 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "animate.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faShoppingCart);
+library.add(fas);
 
 Vue.config.productionTip = false;
 
